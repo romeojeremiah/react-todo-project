de54a11ea182c0c79398fc136055c6043290e3e4
Romeo Jeremiah <romeoclayton@gmail.com>
2022-08-25 12:45:53 -0400

    Delete src directory

--- src/Alert.js
+++
@@ -1,8 +0,0 @@
-import React, { useEffect } from "react";
-
-const Alert = (props) => {
-    const { alertType, alertText } = props.alert;
-    return <p className={`alert alert-${alertType}`}>{alertText}</p>;
-};
-
-export default Alert;
